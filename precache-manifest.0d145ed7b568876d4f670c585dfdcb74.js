c19f59ccfb417992efdbd638e986409e846abfc0
Pooja Choudhary <poojachoudhary560@gmail.com>
2019-06-17 21:03:58 +0530

    Updates

---
+++ precache-manifest.0d145ed7b568876d4f670c585dfdcb74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66f8460dc091858b76bcf1509befebac",
+    "url": "/naspro/index.html"
+  },
+  {
+    "revision": "b3a7414bb3fde7fd5a3a",
+    "url": "/naspro/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/naspro/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "b3a7414bb3fde7fd5a3a",
+    "url": "/naspro/static/js/main.8fd347f2.chunk.js"
+  },
+  {
+    "revision": "4e67f91f98c94fc73138",
+    "url": "/naspro/static/js/runtime~main.306af3ad.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/naspro/static/media/logo.5d5d9eef.svg"
+  }
+]);
